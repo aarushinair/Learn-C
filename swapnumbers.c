1ecfc8389f02b9d19296d04b0df2cd82a69ef345
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 16:40:12 +0530

    Create swapnumbers.c

---
+++ swapnumbers.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main() {
+      double first, second, temp;
+      printf("Enter first number: ");
+      scanf("%lf", &first);
+      printf("Enter second number: ");
+      scanf("%lf", &second);
+
+      // Value of first is assigned to temp
+      temp = first;
+
+      // Value of second is assigned to first
+      first = second;
+
+      // Value of temp (initial value of first) is assigned to second
+      second = temp;
+
+      // %.2lf displays number up to 2 decimal points
+      printf("\nAfter swapping, firstNumber = %.2lf\n", first);
+      printf("After swapping, secondNumber = %.2lf", second);
+      return 0;
+}
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
