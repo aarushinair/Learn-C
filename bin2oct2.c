d45ffcb135e819e935a1ab52b9c81033537ca86b
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:57:49 +0530

    Create bin2oct2.c

---
+++ bin2oct2.c
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <stdio.h>
+long long convert(int oct);
+int main() {
+    int oct;
+    printf("Enter an octal number: ");
+    scanf("%d", &oct);
+    printf("%d in octal = %lld in binary", oct, convert(oct));
+    return 0;
+}
+
+long long convert(int oct) {
+    int dec = 0, i = 0;
+    long long bin = 0;
+
+    // converting octal to decimal
+    while (oct != 0) {
+        dec += (oct % 10) * pow(8, i);
+        ++i;
+        oct /= 10;
+    }
+    i = 1;
+
+   // converting decimal to binary
+    while (dec != 0) {
+        bin += (dec % 2) * i;
+        dec /= 2;
+        i *= 10;
+    }
+    return bin;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
