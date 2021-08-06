a203753e0fa36db2b9e796f792cfcbca5ed4dd89
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:10:10 +0530

    Create swapnum.c

---
+++ swapnum.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+void cyclicSwap(int *a, int *b, int *c);
+int main() {
+    int a, b, c;
+
+    printf("Enter a, b and c respectively: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("Value before swapping:\n");
+    printf("a = %d \nb = %d \nc = %d\n", a, b, c);
+
+    cyclicSwap(&a, &b, &c);
+
+    printf("Value after swapping:\n");
+    printf("a = %d \nb = %d \nc = %d", a, b, c);
+
+    return 0;
+}
+
+void cyclicSwap(int *n1, int *n2, int *n3) {
+    int temp;
+    // swapping in cyclic order
+    temp = *n2;
+    *n2 = *n1;
+    *n1 = *n3;
+    *n3 = temp;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
