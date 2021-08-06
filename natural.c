e7d870677792318cfb1bab2e5a610fc62e939511
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:53:02 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int addNumbers(int n);
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Sum = %d", addNumbers(num));
+    return 0;
+}
+
+int addNumbers(int n) {
+    if (n != 0)
+        return n + addNumbers(n - 1);
+    else
+        return n;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
