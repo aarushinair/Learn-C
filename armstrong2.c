bb208509e9d8dab79f0e72a3ab0dd8d46511ccb5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:45:47 +0530

    Create armstrong2.c

---
+++ armstrong2.c
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+   int num, originalNum, remainder, n = 0;
+   float result = 0.0;
+
+   printf("Enter an integer: ");
+   scanf("%d", &num);
+
+   originalNum = num;
+
+   // store the number of digits of num in n
+   for (originalNum = num; originalNum != 0; ++n) {
+       originalNum /= 10;
+   }
+
+   for (originalNum = num; originalNum != 0; originalNum /= 10) {
+       remainder = originalNum % 10;
+
+      // store the sum of the power of individual digits in result
+      result += pow(remainder, n);
+   }
+
+   // if num is equal to result, the number is an Armstrong number
+   if ((int)result == num)
+    printf("%d is an Armstrong number.", num);
+   else
+    printf("%d is not an Armstrong number.", num);
+   return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
