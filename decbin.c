c8b8a3ab2f09ec0fa86216f9d6116f756e867839
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:38:18 +0530

    Create decbin.c

---
+++ decbin.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+  int n, c, k;
+
+  printf("Enter an integer in decimal number system\n");
+  scanf("%d", &n);
+
+  printf("%d in binary number system is:\n", n);
+
+  for (c = 31; c >= 0; c--)
+  {
+    k = n >> c;
+
+    if (k & 1)
+      printf("1");
+    else
+      printf("0");
+  }
+
+  printf("\n");
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
