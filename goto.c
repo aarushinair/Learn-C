d2864e684c21e4418971602ef5ee7d38d845015b
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:30:25 +0530

    Create goto.c

---
+++ goto.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+  int n, c = 1;
+
+  scanf("%d", &n);   // It is assumed that n >= 1
+
+  print:  // label
+
+  printf("%d\n", c);
+  c++;
+
+  if (c <= n)
+    goto print;
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
