94d4b7a4d3f9a3f9ff44bdefce23a3b2552d5e0f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:28:43 +0530

    Create assert.c

---
+++ assert.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <assert.h>
+ 
+int main() {
+  int a, b;
+ 
+  printf("Input two integers to divide\n");
+  scanf("%d%d", &a, &b);
+ 
+  assert(b != 0);
+ 
+  printf("%d/%d = %.2f\n", a, b, a/(float)b);
+ 
+  return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
