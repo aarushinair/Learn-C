3625645df6272678aea9c16b17abfec4de8c54ee
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:15:53 +0530

    Create concat.c

---
+++ concat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main() {
+  char s1[100] = "programming ", s2[] = "is awesome";
+  int length, j;
+
+  // store length of s1 in the length variable
+  length = 0;
+  while (s1[length] != '\0') {
+    ++length;
+  }
+
+  // concatenate s2 to s1
+  for (j = 0; s2[j] != '\0'; ++j, ++length) {
+    s1[length] = s2[j];
+  }
+
+  // terminating the s1 string
+  s1[length] = '\0';
+
+  printf("After concatenation: ");
+  puts(s1);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
