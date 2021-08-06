4c15a8254d5a787dca7e6499c60b0f617d7b86a7
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:31:03 +0530

    Create mergearrays.c

---
+++ mergearrays.c
@@ -0,0 +1,82 @@
+// It is assumed a user will enter arrays in ascending order
+#include <stdio.h>
+void merge(int [], int, int [], int, int []);
+
+int main() {
+  int a[100], b[100], m, n, c, sorted[200];
+
+  printf("Input number of elements in first array\n");
+  scanf("%d", &m);
+
+  printf("Input %d integers\n", m);
+  for (c = 0; c < m; c++) {
+    scanf("%d", &a[c]);
+  }
+
+  printf("Input number of elements in second array\n");
+  scanf("%d", &n);
+
+  printf("Input %d integers\n", n);
+  for (c = 0; c < n; c++) {
+    scanf("%d", &b[c]);
+  }
+
+  merge(a, m, b, n, sorted);
+
+  printf("Sorted array:\n");
+
+  for (c = 0; c < m + n; c++) {
+    printf("%d\n", sorted[c]);
+  }
+
+  return 0;
+}
+
+void merge(int a[], int m, int b[], int n, int sorted[]) {
+  int i, j, k;
+
+  j = k = 0;
+
+  for (i = 0; i < m + n;) {
+    if (j < m && k < n) {
+      if (a[j] < b[k]) {
+        sorted[i] = a[j];
+        j++;
+      }
+      else {
+        sorted[i] = b[k];
+        k++;
+      }
+      i++;
+    }
+    else if (j == m) {
+      for (; i < m + n;) {
+        sorted[i] = b[k];
+        k++;
+        i++;
+      }
+    }
+    else {
+      for (; i < m + n;) {
+        sorted[i] = a[j];
+        j++;
+        i++;
+      }
+    }
+  }
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
