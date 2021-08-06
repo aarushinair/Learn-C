966e954ce3a2f437f973f7c74408fa7b055fa0a9
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:41:11 +0530

    Create countnum.c

---
+++ countnum.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+    long long n;
+    int count = 0;
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+ 
+    // iterate until n becomes 0
+    // remove last digit from n in each iteration
+    // increase count by 1 in each iteration
+    while (n != 0) {
+        n /= 10;     // n = n/10
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
