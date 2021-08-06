2344c1d16ee46ef02e1ff9eeff77e5fb06ed5fb5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:12:59 +0530

    Create findlargnum.c

---
+++ findlargnum.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n;
+  double *data;
+  printf("Enter the total number of elements: ");
+  scanf("%d", &n);
+
+  // Allocating memory for n elements
+  data = (double *)calloc(n, sizeof(double));
+  if (data == NULL) {
+  printf("Error!!! memory not allocated.");
+  exit(0);
+  }
+
+  // Storing numbers entered by the user.
+  for (int i = 0; i < n; ++i) {
+  printf("Enter number%d: ", i + 1);
+  scanf("%lf", data + i);
+  }
+
+  // Finding the largest number
+  for (int i = 1; i < n; ++i) {
+    if (*data < *(data + i)) {
+      *data = *(data + i);
+    }
+  }
+  printf("Largest number = %.2lf", *data);
+
+  free(data);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
