60c34504945d1f4d846a33fb5a7452f840b842eb
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:22:35 +0530

    Create showsource.c

---
+++ showsource.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+    FILE *fp;
+    int c;
+   
+    // open the current input file
+    fp = fopen(__FILE__,"r");
+
+    do {
+         c = getc(fp);   // read character 
+         putchar(c);     // display character
+    }
+    while(c != EOF);  // loop until the end of file is reached
+    
+    fclose(fp);
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
