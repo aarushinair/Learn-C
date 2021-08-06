f02b22f8213da7cc65b3ee3d21f6f57964586fbc
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:37:47 +0530

    Create insertsubstring.c

---
+++ insertsubstring.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void insert_substring(char*, char*, int);
+char* substring(char*, int, int);
+
+int main()
+{
+   char text[100], substring[100];
+   int position;
+   
+   printf("Enter some text\n");
+   gets(text);
+   
+   printf("Enter a string to insert\n");
+   gets(substring);
+   
+   printf("Enter the position to insert\n");
+   scanf("%d", &position);
+   
+   insert_substring(text, substring, position);
+   
+   printf("%s\n",text);
+   
+   return 0;
+}
+
+void insert_substring(char *a, char *b, int position)
+{
+   char *f, *e;
+   int length;
+   
+   length = strlen(a);
+   
+   f = substring(a, 1, position - 1 );      
+   e = substring(a, position, length-position+1);
+
+   strcpy(a, "");
+   strcat(a, f);
+   free(f);
+   strcat(a, b);
+   strcat(a, e);
+   free(e);
+}
+
+char *substring(char *string, int position, int length)
+{
+   char *pointer;
+   int c;
+ 
+   pointer = malloc(length+1);
+   
+   if( pointer == NULL )
+       exit(EXIT_FAILURE);
+ 
+   for( c = 0 ; c < length ; c++ )
+      *(pointer+c) = *((string+position-1)+c);      
+       
+   *(pointer+c) = '\0';
+ 
+   return pointer;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
