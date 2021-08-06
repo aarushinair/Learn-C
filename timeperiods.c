c0ee1c174f58cb0bf152dd8b98131d382e3863b5
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:19:25 +0530

    Create timeperiods.c

---
+++ timeperiods.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+struct TIME {
+   int seconds;
+   int minutes;
+   int hours;
+};
+
+void differenceBetweenTimePeriod(struct TIME t1,
+                                 struct TIME t2,
+                                 struct TIME *diff);
+
+int main() {
+   struct TIME startTime, stopTime, diff;
+
+   printf("Enter the start time. \n");
+   printf("Enter hours, minutes and seconds: ");
+   scanf("%d %d %d", &startTime.hours,
+         &startTime.minutes,
+         &startTime.seconds);
+
+   printf("Enter the stop time. \n");
+   printf("Enter hours, minutes and seconds: ");
+   scanf("%d %d %d", &stopTime.hours,
+         &stopTime.minutes,
+         &stopTime.seconds);
+
+   // Difference between start and stop time
+   differenceBetweenTimePeriod(startTime, stopTime, &diff);
+   printf("\nTime Difference: %d:%d:%d - ", startTime.hours,
+          startTime.minutes,
+          startTime.seconds);
+   printf("%d:%d:%d ", stopTime.hours,
+          stopTime.minutes,
+          stopTime.seconds);
+   printf("= %d:%d:%d\n", diff.hours,
+          diff.minutes,
+          diff.seconds);
+   return 0;
+}
+
+// Computes difference between time periods
+void differenceBetweenTimePeriod(struct TIME start,
+                                 struct TIME stop,
+                                 struct TIME *diff) {
+   while (stop.seconds > start.seconds) {
+      --start.minutes;
+      start.seconds += 60;
+   }
+   diff->seconds = start.seconds - stop.seconds;
+   while (stop.minutes > start.minutes) {
+      --start.hours;
+      start.minutes += 60;
+   }
+   diff->minutes = start.minutes - stop.minutes;
+   diff->hours = start.hours - stop.hours;
+}
+
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
