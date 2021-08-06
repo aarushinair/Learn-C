9611ce2ab06ff243a53e518e333685019b805058
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:42:14 +0530

    Create numberpower.c

---
+++ numberpower.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    int base, exp;
+    long double result = 1.0;
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+    printf("Enter an exponent: ");
+    scanf("%d", &exp);
+
+    while (exp != 0) {
+        result *= base;
+        --exp;
+    }
+    printf("Answer = %.0Lf", result);
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
