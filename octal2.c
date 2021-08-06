1fbe5e7a0077f83210758e4e7c2ba6ded2c4fc67
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:56:53 +0530

    Create octal2.c

---
+++ octal2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+long long convertOctalToDecimal(int octalNumber);
+int main()
+{
+    int octalNumber;
+
+    printf("Enter an octal number: ");
+    scanf("%d", &octalNumber);
+
+    printf("%d in octal = %lld in decimal", octalNumber, convertOctalToDecimal(octalNumber));
+
+    return 0;
+}
+
+long long convertOctalToDecimal(int octalNumber)
+{
+    int decimalNumber = 0, i = 0;
+
+    while(octalNumber != 0)
+    {
+        decimalNumber += (octalNumber%10) * pow(8,i);
+        ++i;
+        octalNumber/=10;
+    }
+
+    i = 1;
+
+    return decimalNumber;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
