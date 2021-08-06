4408f23a96044a3d3ea156d79ec08e7d6f39e2df
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:39:31 +0530

    Create lcmgcd.c

---
+++ lcmgcd.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main() {
+    int n1, n2, i, gcd, lcm;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    for (i = 1; i <= n1 && i <= n2; ++i) {
+        
+        // check if i is a factor of both integers
+        if (n1 % i == 0 && n2 % i == 0)
+            gcd = i;
+    }
+
+    lcm = (n1 * n2) / gcd;
+
+    printf("The LCM of two numbers %d and %d is %d.", n1, n2, lcm);
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
