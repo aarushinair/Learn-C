69beaff7e57d9940afacbe01b1c558d1e7d69e85
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:20:29 +0530

    Create datastru.c

---
+++ datastru.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct course {
+  int marks;
+  char subject[30];
+};
+
+int main() {
+  struct course *ptr;
+  int noOfRecords;
+  printf("Enter the number of records: ");
+  scanf("%d", &noOfRecords);
+
+  // Memory allocation for noOfRecords structures
+  ptr = (struct course *)malloc(noOfRecords * sizeof(struct course));
+  for (int i = 0; i < noOfRecords; ++i) {
+    printf("Enter subject and marks:\n");
+    scanf("%s %d", (ptr + i)->subject, &(ptr + i)->marks);
+  }
+
+  printf("Displaying Information:\n");
+  for (int i = 0; i < noOfRecords; ++i) {
+    printf("%s\t%d\n", (ptr + i)->subject, (ptr + i)->marks);
+  }
+
+  free(ptr);
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
