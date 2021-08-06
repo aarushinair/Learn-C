3264b0e3d0e335b48f69d0c7c6b951027f7c0260
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:14:12 +0530

    Create countvowels.c

---
+++ countvowels.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main() {
+    char line[150];
+    int vowels, consonant, digit, space;
+
+    vowels = consonant = digit = space = 0;
+
+    printf("Enter a line of string: ");
+    fgets(line, sizeof(line), stdin);
+
+    for (int i = 0; line[i] != '\0'; ++i) {
+        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+            line[i] == 'o' || line[i] == 'u' || line[i] == 'A' ||
+            line[i] == 'E' || line[i] == 'I' || line[i] == 'O' ||
+            line[i] == 'U') {
+            ++vowels;
+        } else if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z')) {
+            ++consonant;
+        } else if (line[i] >= '0' && line[i] <= '9') {
+            ++digit;
+        } else if (line[i] == ' ') {
+            ++space;
+        }
+    }
+
+    printf("Vowels: %d", vowels);
+    printf("\nConsonants: %d", consonant);
+    printf("\nDigits: %d", digit);
+    printf("\nWhite spaces: %d", space);
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
