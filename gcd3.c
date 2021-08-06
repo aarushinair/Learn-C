ef7d4bf3a54b2f077fad3eb54beb302dbb19559c
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:38:35 +0530

    Create gcd3.c

---
+++ gcd3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d",&n1,&n2);
+
+    // if user enters negative number, sign of the number is changed to positive
+    n1 = ( n1 > 0) ? n1 : -n1;
+    n2 = ( n2 > 0) ? n2 : -n2;
+
+    while(n1!=n2)
+    {
+        if(n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+    printf("GCD = %d",n1);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
