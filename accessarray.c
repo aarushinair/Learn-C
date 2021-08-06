751e590bfbe3cd50c02df5647a7889e041b2942b
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:09:32 +0530

    Create accessarray.c

---
+++ accessarray.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+    int data[5];
+
+    printf("Enter elements: ");
+    for (int i = 0; i < 5; ++i)
+        scanf("%d", data + i);
+
+    printf("You entered: \n");
+    for (int i = 0; i < 5; ++i)
+        printf("%d\n", *(data + i));
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
