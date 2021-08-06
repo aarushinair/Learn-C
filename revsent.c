5d22e26d911bd9943fc905640184a796c2cda10e
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:58:22 +0530

    Create revsent.c

---
+++ revsent.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void reverseSentence();
+int main() {
+    printf("Enter a sentence: ");
+    reverseSentence();
+    return 0;
+}
+
+void reverseSentence() {
+    char c;
+    scanf("%c", &c);
+    if (c != '\n') {
+        reverseSentence();
+        printf("%c", c);
+    }
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
