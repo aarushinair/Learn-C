82b906fef1e6f6aaf59e063efd8264e0be6b699f
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:37:12 +0530

    Create patternmatching.c

---
+++ patternmatching.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+int match(char [], char []);
+
+int main() {
+  char a[100], b[100];
+  int position;
+
+  printf("Enter some text\n");
+  gets(a);
+
+  printf("Enter a string to find\n");
+  gets(b);
+
+  position = match(a, b);
+
+  if (position != -1) {
+    printf("Found at location: %d\n", position + 1);
+  }
+  else {
+    printf("Not found.\n");
+  }
+
+  return 0;
+}
+
+int match(char text[], char pattern[]) {
+  int c, d, e, text_length, pattern_length, position = -1;
+
+  text_length    = strlen(text);
+  pattern_length = strlen(pattern);
+
+  if (pattern_length > text_length) {
+    return -1;
+  }
+
+  for (c = 0; c <= text_length - pattern_length; c++) {
+    position = e = c;
+
+    for (d = 0; d < pattern_length; d++) {
+      if (pattern[d] == text[e]) {
+        e++;
+      }
+      else {
+        break;
+      }
+    }
+    if (d == pattern_length) {
+      return position;
+    }
+  }
+
+  return -1;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
