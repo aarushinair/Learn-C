60ee62c163f21c6971b66797450c6792e279f8e0
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 18:14:47 +0530

    Create rmchar.c

---
+++ rmchar.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main() {
+   char line[150];
+   
+   printf("Enter a string: ");
+   fgets(line, sizeof(line), stdin); // take input
+
+
+   for (int i = 0, j; line[i] != '\0'; ++i) {
+
+      // enter the loop if the character is not an alphabet
+      // and not the null character
+      while (!(line[i] >= 'a' && line[i] <= 'z') && !(line[i] >= 'A' && line[i] <= 'Z') && !(line[i] == '\0')) {
+         for (j = i; line[j] != '\0'; ++j) {
+
+            // if jth element of line is not an alphabet,
+            // assign the value of (j+1)th element to the jth element
+            line[j] = line[j + 1];
+         }
+         line[j] = '\0';
+      }
+   }
+   printf("Output String: ");
+   puts(line);
+   return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
