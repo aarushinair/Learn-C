7359061cfa890f5885892ed0611184e6fd8c0e74
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:44:24 +0530

    Create intervals.c

---
+++ intervals.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+   int low, high, i, flag;
+   printf("Enter two numbers(intervals): ");
+   scanf("%d %d", &low, &high);
+   printf("Prime numbers between %d and %d are: ", low, high);
+
+   // iteration until low is not equal to high
+   while (low < high) {
+      flag = 0;
+
+      // ignore numbers less than 2
+      if (low <= 1) {
+         ++low;
+         continue;
+      }
+
+      // if low is a non-prime number, flag will be 1
+      for (i = 2; i <= low / 2; ++i) {
+
+         if (low % i == 0) {
+            flag = 1;
+            break;
+         }
+      }
+
+      if (flag == 0)
+         printf("%d ", low);
+
+      // to check prime for the next number
+      // increase low by 1
+      ++low;
+   }
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
