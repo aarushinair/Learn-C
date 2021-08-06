6cd486a0d791f3dd2e69c115c19e593e6a4b3a88
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 16:39:34 +0530

    Create longkeyword.c

---
+++ longkeyword.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() {
+    int a;
+    long b;   // equivalent to long int b;
+    long long c;  // equivalent to long long int c;
+    double e;
+    long double f;
+
+    printf("Size of int = %zu bytes \n", sizeof(a));
+    printf("Size of long int = %zu bytes\n", sizeof(b));
+    printf("Size of long long int = %zu bytes\n", sizeof(c));
+    printf("Size of double = %zu bytes\n", sizeof(e));
+    printf("Size of long double = %zu bytes\n", sizeof(f));
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
