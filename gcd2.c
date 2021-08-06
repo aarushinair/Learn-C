4c7b2884543f22f6c12da4764665772d7be7c981
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:38:05 +0530

    Create gcd2.c

---
+++ gcd2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2;
+    
+    printf("Enter two positive integers: ");
+    scanf("%d %d",&n1,&n2);
+
+    while(n1!=n2)
+    {
+        if(n1 > n2)
+            n1 -= n2;
+        else
+            n2 -= n1;
+    }
+    printf("GCD = %d",n1);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
