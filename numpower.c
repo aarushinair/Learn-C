fa8ef2a47807294da334b492de4c5f4989da56f2
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2021-08-06 17:42:36 +0530

    Create numpower.c

---
+++ numpower.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    double base, exp, result;
+    printf("Enter a base number: ");
+    scanf("%lf", &base);
+    printf("Enter an exponent: ");
+    scanf("%lf", &exp);
+
+    // calculates the power
+    result = pow(base, exp);
+
+    printf("%.1lf^%.1lf = %.2lf", base, exp, result);
+    return 0;
+}
+
+
+
+
+
+
+
+
+👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
+👀 I’m a Computer Science Engineering Student
+💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
+🌱 #GirlsWhoCode #WomenInTech #WomenInIT #WomenInSTEM #CyberSecurity #QuantumComputing #BlockChain #AI #ML
+📫 How to reach me: https://www.linkedin.com/in/aarushinair/
+👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
+🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
