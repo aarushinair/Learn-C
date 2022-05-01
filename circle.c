31437966f76d76e509fedcf56132de93d9c4440c
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-05-01 08:17:56 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -88,10 +88,6 @@ main()
 
 
 
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
