41aebcdeb65754f2338f41a3ede8470b2c2b0d1a
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2022-08-24 11:00:43 +0530

    Update complexnum.c

--- complexnum.c
+++ complexnum.c
@@ -31,11 +31,6 @@ complex add(complex n1, complex n2) {
 
 
 
-
-
-
-
-
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
