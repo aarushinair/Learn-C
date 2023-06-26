5f69cd6fd430d27f823bddbc4ec97c539d2f8f8d
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2023-06-27 00:36:25 +0530

    Update invschool.c

--- invschool.c
+++ invschool.c
@@ -947,7 +947,7 @@ int clscanf()
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -955,3 +955,4 @@ int clscanf()
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
