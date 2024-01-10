2a55140a5ece1573db1f74e025d7f378127fcc86
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-01-10 21:57:43 +0530

    Update multimatrix2.c

--- multimatrix2.c
+++ multimatrix2.c
@@ -107,7 +107,7 @@ void display(int mult[][10], int rowFirst, int columnSecond)
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -115,3 +115,4 @@ void display(int mult[][10], int rowFirst, int columnSecond)
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
