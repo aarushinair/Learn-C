0affeb73999c93b96e092ec2476a1239fc511787
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-10-25 00:22:28 +0530

    Update numnoloops.c

--- numnoloops.c
+++ numnoloops.c
@@ -27,7 +27,7 @@ void print(int s, int n) {
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -35,3 +35,4 @@ void print(int s, int n) {
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
