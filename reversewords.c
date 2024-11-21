91618e43ac61fd49ca254834739cfe020b820316
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-11-21 08:16:42 +0530

    Update reversewords.c

--- reversewords.c
+++ reversewords.c
@@ -82,7 +82,7 @@ void reverse_string(char *t) {
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -90,3 +90,4 @@ void reverse_string(char *t) {
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
