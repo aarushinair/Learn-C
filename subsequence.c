966d7f148c1283e7303390938b4a2d3ca7ff9664
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-12-03 09:15:18 +0530

    Update subsequence.c

--- subsequence.c
+++ subsequence.c
@@ -53,7 +53,7 @@ int check_subsequence (char a[], char b[]) {
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -61,3 +61,4 @@ int check_subsequence (char a[], char b[]) {
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
