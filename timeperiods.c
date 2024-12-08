3765cc6937e8f867979976864107894d0830b9a2
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-12-08 12:47:21 +0530

    Update timeperiods.c

--- timeperiods.c
+++ timeperiods.c
@@ -62,7 +62,7 @@ void differenceBetweenTimePeriod(struct TIME start,
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -70,3 +70,4 @@ void differenceBetweenTimePeriod(struct TIME start,
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
