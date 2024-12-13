8f9ed0e72c431478cc26fbb6d55b76ffb35bb038
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2024-12-13 09:06:19 +0530

    Update webbrowser.c

--- webbrowser.c
+++ webbrowser.c
@@ -103,7 +103,7 @@ void draw()
 
 
 
-
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -111,3 +111,4 @@ void draw()
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
