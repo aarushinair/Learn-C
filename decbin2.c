9d28b834e90c61ad74d98fc69ff1a76daaa51639
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-01-12 10:37:58 +0530

    Update decbin2.c

--- decbin2.c
+++ decbin2.c
@@ -47,6 +47,13 @@ char *decimal_to_binary(int n)
 
 
 
+
+
+
+
+
+
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -54,3 +61,4 @@ char *decimal_to_binary(int n)
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
