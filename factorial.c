1c96bc6623e4b914932df428cee2c8d432369710
Aarushi Nair <51220720+aarushinair@users.noreply.github.com>
2025-01-14 09:00:57 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -21,6 +21,10 @@ int main() {
 
 
 
+
+
+
+/*
 👋 Hi, I’m @aarushinair - Aarushi Nair (she/her/ella)
 👀 I’m a Computer Science Engineering Student
 💞️ I’m looking to collaborate on #java, #python, #R, #applicationdevelopment
@@ -28,3 +32,4 @@ int main() {
 📫 How to reach me: https://www.linkedin.com/in/aarushinair/
 👩‍🏫 YouTube Channel - Code with Aarushi : https://www.youtube.com/channel/UCKj5T1ELHCmkGKujkpqtl7Q
 🙋‍ Follow me on Twitter: https://twitter.com/aarushinair_
+*/
